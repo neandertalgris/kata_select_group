735e62ab95d49a8aaa97ac1964f495cbaecde798
Sebastian Alemán Fierro <sebas.alem.f@gmail.com>
2017-07-01 15:00:26 -0500

    Kata method

--- client/app/scripts/group/controller.js
+++ client/app/scripts/group/controller.js
@@ -1,15 +1,40 @@
 'use strict';
 
 angular.module('Group')
-.controller('group', function ($scope) {
+    .controller('group', function ($scope) {
 
-  $scope.controller_loaded = 'Group loaded!';
+        $scope.controller_loaded = 'Group loaded!';
 
-})
-.config(function ($routeProvider) {
-  $routeProvider
-  .when('/group', {
-    templateUrl: 'scripts/group/views/group.html',
-    controller: 'group'
-  });
-});
+        $scope.sum_two_numbers = function (number_1, number_2) {
+            return number_1 + number_2;
+        };
+
+        $scope.select_list = function (groups) {
+            var list = [];
+            var result = {};
+            angular.forEach(groups, function (value) {
+                var item = value;
+                angular.forEach(groups, function (value) {
+                    if (value.estocolmo_id === item.estocolmo_id || value.londres_id === item.londres_id) {
+                        if (item !== value) {
+                            var key = value.estocolmo_id === item.estocolmo_id ? item.estocolmo_id : item.londres_id;
+                            result[key] = item;
+                        }
+                    }
+                });
+            });
+            angular.forEach(result, function (value, key) {
+                list.push(key * 1);
+            });
+            return list;
+        };
+
+
+    })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/group', {
+                templateUrl: 'scripts/group/views/group.html',
+                controller: 'group'
+            });
+    });
--- client/app/scripts/group/specs/controller_spec.js
+++ client/app/scripts/group/specs/controller_spec.js
@@ -2,50 +2,100 @@
 
 describe('Controller: select group', function () {
 
-  beforeEach(module('Group'));
+    beforeEach(module('Group'));
 
-  var controller;
-  var scope;
+    var controller;
+    var scope;
 
-  beforeEach(inject(function ($rootScope, $controller) {
-    scope = $rootScope.$new();
-    controller = $controller('group', { $scope: scope });
-  }));
-
-  describe('On instance', function () {
-    it('should set "controller_loaded" variable in scope', function () {
-      expect(scope.controller_loaded).toContain('loaded');
-    });
-  });
-
-  describe('when going to /group', function () {
-
-    var route, location, rootScope, httpBackend;
-
-    beforeEach(inject(function ($route, $location, $rootScope, $httpBackend) {
-      route = $route;
-      location = $location;
-      rootScope = $rootScope;
-      httpBackend = $httpBackend;
-
-      httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        controller = $controller('group', {$scope: scope});
     }));
 
-    afterEach(function () {
-      httpBackend.verifyNoOutstandingExpectation();
-      httpBackend.verifyNoOutstandingRequest();
+    describe('On instance', function () {
+        it('should set "controller_loaded" variable in scope', function () {
+            expect(scope.controller_loaded).toContain('loaded');
+        });
+        it('testing function "sum_two_numbers" in scope', function () {
+            expect(scope.sum_two_numbers(10, 20)).toEqual(30);
+            expect(scope.sum_two_numbers(11, 22)).toEqual(33);
+        });
+        //KATA
+        it('testing function "select_list" in scope', function () {
+            expect(scope.select_list([
+                {
+                    estocolmo_id: 1009,
+                    londres_id: 2011
+                },
+                {
+                    estocolmo_id: 1017,
+                    londres_id: 2011
+                }
+            ])).toEqual([2011]);
+        });
+
+        it('testing function "select_list" in scope', function () {
+            expect(scope.select_list([
+                {
+                    estocolmo_id: 1019,
+                    londres_id: 2021
+                },
+                {
+                    estocolmo_id: 1018,
+                    londres_id: 2021
+                }
+            ])).toEqual([2021]);
+        });
+        it('testing function "select_list" in scope', function () {
+            expect(scope.select_list([
+                {
+                    estocolmo_id: 1009,
+                    londres_id: 2000
+                },
+                {
+                    estocolmo_id: 1009,
+                    londres_id: 2001
+                },
+                {
+                    estocolmo_id: 1002,
+                    londres_id: 2002
+                },
+                {
+                    estocolmo_id: 1003,
+                    londres_id: 2002
+                }
+            ])).toEqual([1009, 2002]);
+        });
     });
 
-    it('should use minesweeper.html and controller', function () {
-      expect(route.current).toBeUndefined();
+    describe('when going to /group', function () {
 
-      location.path('/group');
+        var route, location, rootScope, httpBackend;
 
-      httpBackend.flush();
+        beforeEach(inject(function ($route, $location, $rootScope, $httpBackend) {
+            route = $route;
+            location = $location;
+            rootScope = $rootScope;
+            httpBackend = $httpBackend;
 
-      expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
-      expect(route.current.controller).toBe('group');
+            httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+        }));
+
+        afterEach(function () {
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should use minesweeper.html and controller', function () {
+            expect(route.current).toBeUndefined();
+
+            location.path('/group');
+
+            httpBackend.flush();
+
+            expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
+            expect(route.current.controller).toBe('group');
+        });
     });
-  });
 
 });
